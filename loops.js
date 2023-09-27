de9c2d296bd1da7fe64052aa440550d0cf333683
samson ogbonna <samsonogbonna95@gmail.com>
2023-09-27 09:44:19 +0100

    added more style

---
+++ loops.js
@@ -0,0 +1,20 @@
+//code that lists out all the even numbers from 1...100//
+
+var count = 0;
+for (var i = 0; i <= 100; i++) {
+  console.log(i);
+}
+for (var i = 1; i <= 100; i++) {
+  console.log(+".");
+}
+
+//code that lists out all the prime numbers from 1...100//
+
+let i, y;
+for (y = 2; y <= 100; y++) {
+  if (y % i == 0) count++;
+}
+if (count == 2) console.log(y);
+count = 0;
+
+//code that list out numbers divisible by 5 from 1....1000//
